814f9d2195bc123c72a87c9394f2ef9f0d795185
zxcтема <artsadovin@yandex.ru>
2023-08-26 23:51:26 +0300

    q

---
+++ WishList/DBRepository/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DBRepository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options) 
+        { 
+            
+        }
+
+        DbSet<Wish> Wishes { get; set; }
+    }
+}
